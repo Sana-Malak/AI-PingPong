8bbca9ea684d4b9aa9c6c4e83f72fd293920d710
Sana-Malak <94910194+Sana-Malak@users.noreply.github.com>
2023-04-03 18:08:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
+img=""
+noseX= 0;
+noseY= 0;
+mario=325;
+mario=325;
 
-function setup() {
-createCanvas(1240,336);
-    canvas.center();
+function preload()
+{
+    img=loadImage("paddle05.svg")
 }
+
+function setup()
+{
+    createCanvas(650, 400);
+    video=createCapture(VIDEO);
+    video.size(600,300);
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+    video = createCapture(VIDEO);
+    video.size(800,400);
+    video.parent('game_console');
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background("#D3D3D3");
+    if(noseX < 300)
+    {
+        paddleX=paddleX - 1;
+    }
+    if(noseY < 150)
+    {
+        paddleY=paddleY - 1;
+    }
+    image(img,marioX,marioY, 40,70)
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!')
+}
+
 
